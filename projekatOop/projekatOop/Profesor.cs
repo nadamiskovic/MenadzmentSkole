90f1b34b9f0e0be34806296b26b0a4ee731a0f8a
kostico <ognjen.kostic@gimnazijasm.edu.rs>
2026-03-04 11:19:52 +0100

    Refactor Profesor class to include properties and constructors

--- projekatOop/projekatOop/Profesor.cs
+++ projekatOop/projekatOop/Profesor.cs
@@ -1,17 +1,29 @@
-﻿using System;
+using System;
 
 namespace projekatOop
 {
     public class Profesor : Osoba
     {
+        public string Ime { get; set; } = string.Empty;
+        public string Prezime { get; set; } = string.Empty;
+
         public Guid Id { get; private set; } 
         public string Predmet { get; set; } = string.Empty;
 
         public Profesor(string ime, string prezime, string predmet)
-            : base(ime, prezime)
         {
-            this.Id = Guid.NewGuid();
-            this.Predmet = predmet;
+            Id = Guid.NewGuid();
+            Ime = ime;
+            Prezime = prezime;
+            Predmet = predmet;
+        }
+
+        public Profesor()
+        {
+            Id = Guid.NewGuid();
+            Ime = string.Empty;
+            Prezime = string.Empty;
+            Predmet = string.Empty;
         }
 
         public string IspisiPodatke()
